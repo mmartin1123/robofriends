f48b2990388f7fb9f2135aec259b8274b18986bd
Michael Martin <mictin3@gmail.com>
2019-10-21 20:42:01 -0600

    Updates

---
+++ precache-manifest.6bff127d9febbc66bd0854d02e68e5ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad7ec7aec0164ce97c0d6afd8b6430a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7b39d04e570cb648875d",
+    "url": "/robofriends/static/css/main.1f393a31.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "7b39d04e570cb648875d",
+    "url": "/robofriends/static/js/main.9ec7460a.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
